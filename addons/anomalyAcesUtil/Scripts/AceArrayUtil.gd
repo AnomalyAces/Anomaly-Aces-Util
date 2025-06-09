1983b31d26e29b88fe909bb4edeb2a53e5229dd8
Anomaly Aces <anomalyaces@gmail.com>
2025-06-09 18:46:32 -0400

    local changes

--- addons/anomalyAcesUtil/Scripts/AceArrayUtil.gd
+++ addons/anomalyAcesUtil/Scripts/AceArrayUtil.gd
@@ -1,8 +1,16 @@
-class_name AceArrayUtil extends Node
+@tool
+extends Node
 
-## Find First Element of Array that me
-static func findFirst( arr:Array, search_criteria: Callable) -> Variant: 
+## Find First Element of Array that meets the criteria
+func findFirst( arr:Array, search_criteria: Callable) -> Variant: 
 	for item in arr:
 		if search_criteria.call(item):
 			return item
 	return null
+
+func addUnique(arr: Array, item: Variant) -> bool:
+	if not arr.has(item):
+		arr.push_back(item)
+		return true
+	else:
+		return false
--- addons/anomalyAcesUtil/anomaly_aces_util_plugin.gd
+++ addons/anomalyAcesUtil/anomaly_aces_util_plugin.gd
@@ -3,13 +3,11 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	pass
 	# Initialization of the plugin goes here.
 	#Add Autoloads
-	#add_autoload_singleton("AceArrayUtil", "res://addons/anomalyAcesUtil/Scripts/AceArrayUtil.gd")
-	#pass
+	add_autoload_singleton("AceArrayUtil", "res://addons/anomalyAcesUtil/Scripts/AceArrayUtil.gd")
+	pass
 
 
 func _exit_tree():
-	pass
-	#remove_autoload_singleton("AceArrayUtil")
+	remove_autoload_singleton("AceArrayUtil")

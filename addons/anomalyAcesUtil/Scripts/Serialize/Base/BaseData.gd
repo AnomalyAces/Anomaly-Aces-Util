86ee0713f3cd7e805c5c3d69fd0f7f6eac9e1c99
Anomaly Aces <anomalyaces@gmail.com>
2025-11-25 23:37:00 -0500

    removing old files

--- addons/anomalyAcesUtil/Scripts/Serialize/Base/BaseData.gd
+++
@@ -1,8 +0,0 @@
-class_name BaseData extends Node
-
-func _get_properties() -> Array[String]:
-	var properties: Array[String] 
-	properties.assign( get_script().get_script_property_list().map(
-		func(prop) -> String: return prop.name 
-	))
-	return properties
--- addons/anomalyAcesUtil/Scripts/Serialize/Base/BaseResource.gd
+++
@@ -1,8 +0,0 @@
-class_name BaseResource extends Resource
-
-func _get_properties() -> Array[String]:
-	var properties: Array[String] 
-	properties.assign( get_script().get_script_property_list().map(
-		func(prop) -> String: return prop.name 
-	))
-	return properties
